c1f1d6ed6b7a6f27f39309f8cbb5110ef98d104e
Damian <damian@mindsharestudios.com>
2014-12-26 15:20:40 -0700

    Update update-aliases.sh

--- scripts/update-aliases.sh
+++ scripts/update-aliases.sh
@@ -1,4 +1,4 @@
-(labs && cd bash && git pull && cp -i ./user/.bash_aliases ~/.bash_aliases)
+(cd /Volumes/mindshare/Labs\ Projects/bash && git pull && cp -i ./user/.bash_aliases ~/.bash_aliases)
 source ~/.bashrc
 echo "Aliases updated:"
 echo
