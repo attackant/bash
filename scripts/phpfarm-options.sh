dd3cde2c68bb0c20d845260565fb8f4d3a6ef85a
Damian Taggart <damian@mindsharestudios.com>
2014-12-30 21:25:26 -0700

    add options for phpfarm

---
+++ scripts/phpfarm-options.sh
@@ -0,0 +1,52 @@
+# rename this to custom-options.sh in the phpfarm/src
+# directory before compiling with phpfarm
+
+configoptions="\ 
+--with-libdir=lib64 \ 
+--enable-memory-limit \ 
+--with-regex=php \ 
+--enable-sysvsem \ 
+--enable-sysvshm \ 
+--enable-sysvmsg \ 
+--enable-track-vars \ 
+--enable-trans-sid \ 
+--with-bz2 \ 
+--enable-ctype \ 
+--without-gdbm \ 
+--with-iconv \ 
+--enable-filepro \ 
+--enable-shmop \ 
+--with-libxml-dir=/usr \ 
+--with-kerberos=/usr \ 
+--with-openssl \ 
+--enable-dbx \ 
+--with-system-tzdata \ 
+--with-mysql=/usr \ 
+--with-mysqli=/usr/bin/mysql_config \ 
+--enable-pdo \ --with-pdo-mysql=/usr \ 
+--enable-fastcgi \ 
+--enable-force-cgi-redirect \ 
+--with-curl \ 
+--enable-bcmath \ 
+--enable-calendar \ 
+--enable-exif \ 
+--enable-ftp \ 
+--with-gd \ 
+--with-jpeg-dir=/usr \ 
+--with-png-dir \ 
+--with-freetype-dir=/usr \ 
+--with-t1lib \ 
+--with-zlib-dir=/usr \ 
+--with-gettext=/usr \ 
+--enable-mbstring \ 
+--with-mcrypt=/usr \ 
+--with-mhash \ 
+--with-mime-magic \ 
+--enable-soap \ 
+--enable-sockets \ 
+--with-tidy \ 
+--enable-wddx \ 
+--with-xsl=/usr \ 
+--with-zip \ 
+--with-zlib \ 
+--enable-zip \
