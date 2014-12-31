be15ab00e56151bc9d469b1dcf9489a2752ef677
Damian Taggart <damian@mindsharestudios.com>
2014-12-31 15:38:16 -0700

    fixes for phpfarm

--- scripts/phpfarm-options.sh
+++ scripts/phpfarm-options.sh
@@ -1,52 +1,45 @@
-# rename this to custom-options.sh in the phpfarm/src
-# directory before compiling with phpfarm
+# rename this to custom-options.sh in the phpfarm/src directory 
+# before compiling with phpfarm
 
-configoptions="\ 
---with-libdir=lib64 \ 
---enable-memory-limit \ 
---with-regex=php \ 
---enable-sysvsem \ 
---enable-sysvshm \ 
---enable-sysvmsg \ 
---enable-track-vars \ 
---enable-trans-sid \ 
---with-bz2 \ 
---enable-ctype \ 
---without-gdbm \ 
---with-iconv \ 
---enable-filepro \ 
---enable-shmop \ 
---with-libxml-dir=/usr \ 
---with-kerberos=/usr \ 
---with-openssl \ 
---enable-dbx \ 
---with-system-tzdata \ 
---with-mysql=/usr \ 
---with-mysqli=/usr/bin/mysql_config \ 
---enable-pdo \ --with-pdo-mysql=/usr \ 
---enable-fastcgi \ 
---enable-force-cgi-redirect \ 
---with-curl \ 
---enable-bcmath \ 
---enable-calendar \ 
---enable-exif \ 
---enable-ftp \ 
---with-gd \ 
---with-jpeg-dir=/usr \ 
---with-png-dir \ 
---with-freetype-dir=/usr \ 
---with-t1lib \ 
---with-zlib-dir=/usr \ 
---with-gettext=/usr \ 
---enable-mbstring \ 
---with-mcrypt=/usr \ 
---with-mhash \ 
---with-mime-magic \ 
---enable-soap \ 
---enable-sockets \ 
---with-tidy \ 
---enable-wddx \ 
---with-xsl=/usr \ 
---with-zip \ 
---with-zlib \ 
+
+configoptions="\
+--with-libdir=/usr \
+--with-mysql=/usr
+--with-mysqli=/usr/bin/mysql/bin/mysql_config \
+--with-pdo_mysql=/usr/bin/mysql \
+--with-regex=php \
+--with-bz2 \
+--enable-ctype \
+--without-gdbm \
+--with-iconv \
+--enable-shmop \
+--with-libxml-dir=/usr \
+--with-kerberos=/usr \
+--enable-pdo \
+--with-curl \
+--enable-bcmath \
+--with-gd \
+--with-zlib-dir=/usr \
+--with-gettext=/usr \
+--with-mcrypt=/usr \
+--with-mhash \
+--with-tidy \
+--enable-wddx \
+--with-xsl=/usr \
+--enable-debug \
+--disable-short-tags \
+--without-pear \
+--enable-calendar \
+--enable-exif \
+--enable-ftp \
+--enable-mbstring \
+--enable-pcntl \
+--enable-soap \
+--enable-sockets \
+--enable-sqlite-utf8 \
+--enable-wddx \
 --enable-zip \
+--with-zlib \
+--with-gettext \
+--with-jpeg-dir=/usr \
+"
