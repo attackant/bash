45cc22bc20be87b597f3bcf03413b974ac537555
Damian Taggart <damian@mindsharestudios.com>
2015-09-07 22:20:24 -0600

    alias scripte imrovement

--- scripts/update-aliases.sh
+++ scripts/update-aliases.sh
@@ -1,3 +1,8 @@
 (cd /Volumes/mindshare/Labs\ Projects/bash && git pull && cp -iv ./user/.bash_aliases ~/.bash_aliases)
+
+if [ ! -f ~/.bash_aliases_local ]; then
+    touch ~/.bash_aliases_local
+fi
+
 source ~/.bashrc
 echo "Aliases updated."
