b91fabb60fd79d2a9327f7c71f8558bdafd1609b
Damian <damian@mindsharestudios.com>
2015-11-25 15:04:15 -0700

    rm volumes

--- scripts/update-aliases.sh
+++ scripts/update-aliases.sh
@@ -1,4 +1,4 @@
-(cd /Volumes/mindshare/Labs\ Projects/bash && git pull && cp -iv ./user/.bash_aliases ~/.bash_aliases)
+(cd ~/Labs\ Projects/bash && git pull && cp -iv ./user/.bash_aliases ~/.bash_aliases)
 
 if [ ! -f ~/.bash_aliases_local ]; then
     touch ~/.bash_aliases_local
