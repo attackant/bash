09c417f2c6015ec8ad78321134d6859adaf28f3c
Damian Taggart <damian@mindsharelabs.com>
2016-04-27 22:19:30 -0600

    cleanup

--- scripts/install-wp.sh
+++ scripts/install-wp.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 wp core download && wp core config --dbname=wordpress --dbuser=root --dbpass=
 
-wp core install --admin_email=info@mindsharestudios.com --admin_password=17dNIluyfSmM --url=https://wordpress.dev --title="Mindshare Labs Dev" --admin_user=mindshare
+wp core install --admin_email=info@mindsharelabs.com --admin_password=17dNIluyfSmM --url=https://wordpress.dev --title="Mindshare Labs Dev" --admin_user=mindshare
 
-wp plugin install --activate --force admin-menu-editor broken-link-checker developer wp-dbmanager php-browser-detection simple-history tinymce-advanced wordpress-https wordpress-seo https://server.mind.sh:8081/mindshare-labs/mindshare-client-security/repository/archive.zip https://github.com/mindsharestudios/mindshare-api/archive/master.zip https://mind.sh/are/media/plugins/advanced-custom-fields-pro-b3JkZXJfaWQ9MzI5NTN8dHlwZT1kZXZlbG9wZXJ8ZGF0ZT0yMDE0LTA3LTA3IDE1OjU4OjE5.zip https://mind.sh/are/media/plugins/gravityforms_25322ade6953d1770a492559697c1480.zip https://mind.sh/are/media/plugins/video-user-manuals-2cnQah9JKsdlMbDmzFgkgS8u6l31sO.zip
+wp plugin install --activate --force admin-menu-editor broken-link-checker developer wp-dbmanager php-browser-detection simple-history tinymce-advanced wordpress-seo plugin-central https://git.mindsharelabs.com/mindshare-labs/mindshare-client-security/repository/archive.zip https://github.com/mindsharelabs/mindshare-api/archive/master.zip
