6f915615f187a7f23f649599236e410031fdba5a
Damian Taggart <damian@attackant>
2023-01-03 12:49:07 -0700

    fix path

--- scripts/update-aliases.sh
+++ scripts/update-aliases.sh
@@ -1,4 +1,4 @@
-(cd ~/Labs\ Projects/bash && git pull && cp -iv ./user/.bash_aliases ~/.bash_aliases)
+(cd ~/projects/bash && git pull && cp -iv ./user/.bash_aliases ~/.bash_aliases)
 
 if [ ! -f ~/.bash_aliases_local ]; then
     touch ~/.bash_aliases_local
